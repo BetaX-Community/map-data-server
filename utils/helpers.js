9e2c28da77c69e815a0e6830966fd8d19370836f
A. Marius Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-09 20:53:14 +0400

    [FIX+FEAT] Solve over-stringified string issue and add eslint (#6)
    * Solve over-stringified strings issue
    
    * Add eslint

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
+function jsonParser(blob) {
+  let parsed = JSON.parse(blob);
+  if (typeof parsed === 'string') parsed = jsonParser(parsed);
+  return parsed;
+}
+
 module.exports={
-    loadData(filename){
-        const dataFolderPath=path.join(__dirname,"..","data")
-        const filePath=path.join(dataFolderPath,filename)
-        return JSON.parse(fs.readFileSync(filePath,"utf-8"));
-    }
+  loadData(filename){
+    const dataFolderPath = path.join(__dirname,"..","data")
+    const filePath = path.join(dataFolderPath,filename)
+    return jsonParser(fs.readFileSync(filePath,"utf-8"));
+  }
 }
