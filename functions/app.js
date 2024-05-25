4477663113c6f330fecead37e07b2edfe4608242
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-25 20:59:47 +0400

    Use /api as base URL in any environment

--- functions/app.js
+++ functions/app.js
@@ -4,17 +4,12 @@ const serverless = require("serverless-http");
 const express = require("express");
 dotenv.config()
 const PORT=process.env.PORT || 2999;
-const env=process.env.ENV || 'prod';
 const app = express();
 const routes = require("../routes/map.route");
 
 app.use(express.json());
 app.use(cors());
-if (env === 'dev') {
-  app.use("/api", routes);
-} else {
-  app.use("/.netlify/functions/app", routes);
-}
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log("Server running on port",PORT);
