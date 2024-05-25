5cafb803a1c545e206e3d1c8cc30f3aa44852a64
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-25 19:36:21 +0400

    Set up deployment to Netlify

--- functions/app.js
+++ functions/app.js
@@ -1,15 +1,23 @@
 const cors=require("cors");
 const dotenv=require("dotenv");
+const serverless = require("serverless-http");
 const express = require("express");
 dotenv.config()
 const PORT=process.env.PORT || 2999;
+const env=process.env.ENV || 'prod';
 const app = express();
-const routes = require("./routes/map.route");
+const routes = require("../routes/map.route");
 
 app.use(express.json());
 app.use(cors());
-app.use("/", routes);
+if (env === 'dev') {
+  app.use("/", routes);
+} else {
+  app.use("/.netlify/functions/app", routes);
+}
 
 app.listen(PORT, () => {
     console.log("Server running on port",PORT);
 });
+
+module.exports.handler = serverless(app);
