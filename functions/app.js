0f40ed758a4f8bca01d2dc3bbd9ef81c5fd79331
Stéphane <chkstephane32@gmail.com>
2024-05-28 20:04:22 +0300

    [Swagger] init swagger

--- functions/app.js
+++ functions/app.js
@@ -1,14 +1,17 @@
 const cors=require("cors");
 const dotenv=require("dotenv");
 const serverless = require("serverless-http");
+const swagger = require('../swagger');
 const express = require("express");
 dotenv.config()
 const PORT=process.env.PORT || 2999;
 const app = express();
 const routes = require("../routes/map.route");
 
+swagger(app)
 app.use(express.json());
 app.use(cors());
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
